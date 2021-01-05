d891fb393efa1800cdf9e7d18ff62bf3ad3348b2
Salih SEKER <salihseker35@gmail.com>
2021-01-05 08:12:43 +0300

    Projenin Oluşturulması

---
+++ IsParkMap/IsParkMap.Entities/Concrete/Otopark.cs
@@ -0,0 +1,37 @@
+﻿using IsParkMap.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsParkMap.Entities.Concrete
+{
+    public class Otopark : ITable
+    {
+        public int _id { get; set; }
+        public int ParkID { get; set; }
+        public string ParkAdi { get; set; }
+        public int LokasyonID { get; set; }
+        public int LokasyonKodu { get; set; }
+        public string LokasyonAdi { get; set; }
+        public int ParkTipiID { get; set; }
+        public string ParkTipi { get; set; }
+        public int ParkKapasitesi { get; set; }
+        public string CalismaSaatleri { get; set; }
+        public int BolgeID { get; set; }
+        public string Bolge { get; set; }
+        public int AltBolgeID { get; set; }
+        public string AltBolge { get; set; }
+        public int IlceID { get; set; }
+        public string Ilce { get; set; }
+        public string Adres { get; set; }
+        public string EnlemBoylam { get; set; }
+        public string PolygonVerisi { get; set; }
+        public float Boylam { get; set; }
+        public float Enlem { get; set; }
+        public float AylikAbonelikUcreti { get; set; }
+        public int UcretsizParklanmaSuresidakika { get; set; }
+        public string Tarifesi { get; set; }
+        public int ParkEtDevamEtNoktasi { get; set; }
+    }
+
+}
